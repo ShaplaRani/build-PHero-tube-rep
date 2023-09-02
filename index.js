c34dc05ee3b3163eea75558c6f17c785322b5978
ShaplaRani <shaplarani621@gmail.com>
2023-09-02 00:29:33 -0700

    added the blog part

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let value;
 const handleCategory = async () => {
    const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
    const data = await response.json();
@@ -6,7 +7,7 @@ const handleCategory = async () => {
    data.data?.forEach((category) =>{
       const div = document.createElement('div');
       div.innerHTML = `
-      <button onclick = "handleLoadVideos('${category?.category_id}')" class="buttonColors font-medium text-lg
+      <button onclick = "handleLoadVideos('${category?.category_id}')" class="button-colors font-medium text-lg
        text-gray-600 bg-gray-200 py-2 md:py-3 px-4 md:px-5 rounded-md">${category.category}</button>
       `;
       buttonContainer.appendChild(div);
@@ -16,23 +17,16 @@ const handleCategory = async () => {
 const handleLoadVideos = async (category_id) =>{
     const response = await fetch(` https://openapi.programming-hero.com/api/videos/category/${category_id}`);
     const data = await response.json();
-   
-   const sortingView = document.getElementById('sorting-view');
-   sortingView.innerHTML= "";
-   const h2 = document.createElement('h2')
-   h2.innerText = category_id ;
-   sortingView.appendChild(h2);
-
+    value = category_id;
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = "";
     const emptyContainer = document.getElementById('empty-card');
     emptyContainer.innerHTML = "";
-    console.log(data.data);
+  
     data.data.length !== 0?data.data.forEach(video => {
-        
      const div = document.createElement('div');
      div.innerHTML = `
-       <div class="card bg-base-100 shadow-xl">
+       <div class="card bg-white ">
         <h2 class = "hidden"> ${category_id} </h2>
         <figure><img class ="h-56 w-full" src="${video.thumbnail}" alt="" /></figure>
         <div class = "absolute top-48 left-60 font-normal bg-black text-xs w-36 text-white text-center">
@@ -40,8 +34,8 @@ const handleLoadVideos = async (category_id) =>{
         </div>
         
         <div class="card-body">
-         <div class = "flex gap-4 items-center"> 
-          <img class = "w-20 h-20 rounded-full" src="${video.authors[0]?.profile_picture}" alt="" />
+         <div class = "flex gap-4 "> 
+          <img class = "w-16 h-16 rounded-full" src="${video.authors[0]?.profile_picture}" alt="" />
            <div>
               <h2 class="font-bold text-base text-gray-900">${video?.title}</h2>
                <div class = "my-3 flex gap-4">
@@ -87,20 +81,14 @@ const emptyArray =() =>{
 document.getElementById('btn-blog').addEventListener('click',function(){
    window.location.href = 'blog.html';
 })
+
 //function call
 handleLoadVideos('1000');
 handleCategory();
 
 //sorting
  const sortView =  ()=>{
-    const  sortingView = document.querySelector('#sorting-view h2');
-    const value = sortingView.innerText;
-    if(value == 1005){
-      emptyArray();
-    }
-    else{
-      sorting(value);
-    }
+   sorting(value);
  };
    
   const sorting =async(id) =>{
@@ -116,18 +104,18 @@ handleCategory();
    cardContainer.innerHTML = "";
     const emptyContainer = document.getElementById('empty-card');
     emptyContainer.innerHTML = "";
-   view.forEach(video => {
+    view.length !== 0?view.forEach(video => {
    const div = document.createElement('div');
     div.innerHTML = `
-      <div class="card bg-base-100 shadow-xl">
+      <div class="card bg-white ">
        <figure><img class ="h-56 w-full" src="${video.thumbnail}" alt="" /></figure>
        <div class = "absolute top-48 left-60 font-normal bg-black text-xs w-36 text-white text-center">
         <p>${video.others?.posted_date?timeConvert(video.others?.posted_date):''}</p>
        </div>
        
        <div class="card-body">
-        <div class = "flex gap-4 items-center"> 
-         <img class = "w-20 h-20 rounded-full" src="${video.authors[0]?.profile_picture}" alt="" />
+        <div class = "flex gap-4"> 
+         <img class = "w-16 h-16 rounded-full" src="${video.authors[0]?.profile_picture}" alt="" />
           <div>
              <h2 class="font-bold text-base text-gray-900">${video?.title}</h2>
               <div class = "my-3 flex gap-4">
@@ -142,6 +130,6 @@ handleCategory();
        `;
    cardContainer.appendChild(div);   
        
-   });
+   }):emptyArray();
 };
 
